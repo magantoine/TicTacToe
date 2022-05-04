8a0745d01efa82d0ccbb35ad0fc0fe613dba81e8
Antoine Magron <antoine.magron@epfl.ch>
2022-05-04 11:54:52 +0200

    adds saved results

--- morpion.py
+++ morpion.py
@@ -8,7 +8,7 @@
 #       format_version: '1.5'
 #       jupytext_version: 1.13.7
 #   kernelspec:
-#     display_name: ann
+#     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
